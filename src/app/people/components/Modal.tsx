dd45b0cc338d208054838813bb75eb0c4a2c364c
Andras Zold <andras.zold@digitalthinkers.com>
2023-09-23 13:39:52 +0200

    adding modal

---
+++ src/app/people/components/Modal.tsx
@@ -0,0 +1,71 @@
+import { Planet, SwapiPersonResult } from "@/types/swapi"
+
+async function getPlanet(planetUrl: string) {
+  const res = await fetch(`${planetUrl}`)
+  return res.json()
+}
+
+const Modal: React.FC<{ person: SwapiPersonResult; onClose: () => void }> = async ({
+  person,
+  onClose,
+}) => {
+  const planet: Planet = person.homeworld && (await getPlanet(person.homeworld))
+
+  const handleBackgroundClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.target === e.currentTarget) {
+      onClose()
+    }
+  }
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div
+        className="modal-bg fixed inset-0 bg-black opacity-50"
+        onClick={handleBackgroundClick}
+      ></div>
+      <div className="modal-content bg-white p-6 rounded-lg shadow-lg relative">
+        <div className="flex flex-col justify-between text-black font-mono">
+          <h2 className="text-xl font-bold">{person.name}</h2>
+          {person.homeworld && (
+            <div>
+              <p>
+                Height: <span className="font-bold">{person.height}</span>
+              </p>
+              <p>
+                Mass: <span className="font-bold">{person.mass}</span>
+              </p>
+              <p>
+                Birth year: <span className="font-bold">{person.birth_year}</span>
+              </p>
+              <p>
+                Movies: <span className="font-bold">{person.films.length}</span>
+              </p>
+              <p>
+                Planet: <span className="font-bold">{planet.name}</span>
+              </p>
+              <p>
+                Planet terrain: <span className="font-bold">{planet.terrain}</span>
+              </p>
+              <p>
+                Planet climate: <span className="font-bold">{planet.climate}</span>
+              </p>
+            </div>
+          )}
+          <button
+            className="text-gray-700 hover:text-gray-900 hover:bg-red-400 rounded mt-4 p-2"
+            onClick={onClose}
+          >
+            Close &#10006;
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Modal
+
+{
+  /* kérdezzük le a szülőföldjét is,
+melynek jelenítsük meg a nevét, terepét (terrain) és klímáját.n */
+}
